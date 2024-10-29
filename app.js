40496475e8477b6d5994e9d27791c9511d2043e9
PutriHabibahDevina <putrihdm13@gmail.com>
2024-10-29 20:36:04 +0700

    CRUD_RESTAPI

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const todoRoutes = require('./routes/todo.js');
 const port = 3000;
 
-app.set('view engine', 'ejs');
+app.use(express.json());
 
+app.use('/todos', todoRoutes);
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
     res.render('index');
 });
